3d255d07ae87cea77633783cb336c1259b87d0ea
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-01-16 06:19:52 +0530

    Misc changes

--- app/src/main/java/com/example/new2/MainActivity.java
+++ app/src/main/java/com/example/new2/MainActivity.java
@@ -3,24 +3,28 @@ package com.example.new2;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import androidx.appcompat.app.AppCompatActivity;
 import eu.chainfire.libsuperuser.Shell;
 
 public class MainActivity extends AppCompatActivity implements StartupCallback{
 
     private static String TAG = MainActivity.class.getSimpleName();
     TextView rootText;
+    String command = "pm list packages | grep -v com.google.* | grep -v com.android.* | grep -v  com.quic.* | grep -v android.*";
 
 
-    private class Startup extends AsyncTask<Void, Void, Void> {
+    private class Startup extends AsyncTask<String, Void, Void> {
         private ProgressDialog dialog = null;
         private Context context = null;
         private boolean suAvailable = false;
@@ -54,26 +58,28 @@ public class MainActivity extends AppCompatActivity implements StartupCallback{
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
             // Let's do some SU stuff
             suAvailable = Shell.SU.available();
             if (suAvailable) {
                 suVersion = Shell.SU.version(false);
                 suVersionInternal = Shell.SU.version(true);
                 suResult = Shell.SU.run(new String[] {
-                        "pm list packages | grep -v com.google.* | grep -v com.android.* | grep -v  com.quic.* "
+                        params[0]
+
                 });
             }
 
             // This is just so you see we had a progress dialog,
             // don't do this in production code
-            try { Thread.sleep(5000); } catch(Exception e) { }
+            try { Thread.sleep(1000); } catch(Exception e) { }
 
             return null;
         }
 
         @Override
         protected void onPostExecute(Void result) {
+            List<String> packages = new ArrayList<>();
             dialog.dismiss();
 
             // output
@@ -84,11 +90,13 @@ public class MainActivity extends AppCompatActivity implements StartupCallback{
                     append((char)10);
             if (suResult != null) {
                 for (String line : suResult) {
-                    sb.append(line).append((char)10);
+                    String x = line.substring(("package").length() + 1);
+                    packages.add(x);
+                    sb.append(x).append((char)10);
                 }
             }
 
-            callback.rootCallback(sb.toString());
+            callback.rootCallback(sb.toString(), packages);
         }
     }
 
@@ -97,17 +105,30 @@ public class MainActivity extends AppCompatActivity implements StartupCallback{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rootText = findViewById(R.id.rootText);
+        rootText =(TextView) findViewById(R.id.rootText);
         rootText.setMovementMethod(new ScrollingMovementMethod());
         //Background stuff
-        new Startup().setContext(this).setListener(this).execute();
+        new Startup().setContext(this).setListener(this).execute(command);
 
     }
 
     @Override
-    public void rootCallback(String text) {
+    public void rootCallback(String text, List<String> packages) {
         this.rootText.setText(text);
+
     }
 
+    List<ApplicationInfo> getPackageNames(){
+        final PackageManager pm = getPackageManager();
+//get a list of installed apps.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
+        for (ApplicationInfo packageInfo : packages) {
+            Log.d(TAG, "Installed package :" + packageInfo.packageName);
+            Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
+            Log.d(TAG, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+        }
+
+        return packages;
+    }
 }
